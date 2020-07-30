f7f1f33e00ccbc57cabac60a4cb50afc4f8ed5e0
Matt Gennings <mattguy19@gmail.com>
2020-07-30 13:26:40 -0400

    we have a working app folks

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
-  // Add a getHeroes method to return the mock heroes.
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
